cd7e15b7b8fc84e609220edfec908a134435a66c
Matt Byrne <mcbyrnewpi@yahoo.com>
2018-02-12 11:10:02 -0500

    Add currency label to text fields

--- window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -12,7 +12,23 @@ import UIKit
 class CurrencyTxtField: UITextField {
     
     override func draw(_ rect: CGRect) {
-        <#code#>
+        let size: CGFloat = 20
+        // create a label for the currency, on left of text field centered vertically
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.8438865754, green: 0.8438865754, blue: 0.8438865754, alpha: 0.7979184503)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        
+        // make sure label fits the current locale and currency
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        
+        // add currency symbol from formatter, and add subview
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
     }
 
     override func prepareForInterfaceBuilder() {
@@ -27,7 +43,10 @@ class CurrencyTxtField: UITextField {
     func customizeView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2537724743)
         layer.cornerRadius = 5.0
-        textAlignment = .center      
+        textAlignment = .center
+        
+        // make sure that overriding draw rect doesn't affect previous styling
+        clipsToBounds = true
    
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
