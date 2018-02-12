6940db8e7fbba074b894a4a2163c344d0c039b09
Matt Byrne <mcbyrnewpi@yahoo.com>
2018-02-12 09:47:22 -0500

    Add assets

---
+++ window-shopper/Controller/MainVC.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Matthew Byrne on 2/8/18.
+//  Copyright © 2018 Matthew Byrne. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.6250713996, blue: 0.1741186037, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+    }
+
+    @objc func calculate() {
+        print("We got here")
+    }
+
+
+}
+
